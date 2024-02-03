a5bb86d8625fd3c16d1dab0ec670a0344161c6f6
오승훈 <seunghun6257@gmail.com>
2024-02-04 03:02:22 +0900

    #2.15 Conditionals part Three

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 //나이 값 가져오기
-let age = parseInt(prompt('How old are you?'));
+let age = parseInt(prompt("만 나이를 입력해주세요"));
 
 if(isNaN(age)){
   alert("숫자를 입력해주세요.")
+} else if ( age < 0) {
+  alert("장난하지마세요^^.")
+} else if (age < 19 ){
+  alert("만 19세 아니면 술 못사요.")
+} else if(age < 19 || age > 70){
+  alert("건강 생각하셔야지요...")
 } else {
-  alert("Thank you.")
+  alert("감사합니다.")
 }
+
+// or = || , && 
+// || 하나만 참이면 OK
+// && 둘다 참이여야 OK
 
 console.log(age)
