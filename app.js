9efc76f335dd28381ff907257bd1f4222355b0c4
오승훈 <seunghun6257@gmail.com>
2024-02-04 21:15:35 +0900

    #3.6 CSS in Javascript

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 const title = document.querySelector("#title")
 console.dir(title)
 
-
-title.style.color = "blue"
-document.querySelector("#title").style.color = "red"
-
-
-title.addEventListener("mousewheel", handleTitleClick)
-
 function handleTitleClick () {
-  title.innerText = "Oh!"
-  title.style.transition = "3s"
-  title.style.color = "blue"
-  title.style.marginLeft = "150px"
+  const titleColor = title.style.color; 
+  let newColor;
+  if(titleColor === "blue") {
+    newColor = "red"
+  } else {
+    newColor = "blue"
+  }
+  title.style.color = newColor
+  console.log(title.style.color)
 }
 
-console.dir(title)
+title.addEventListener("click", handleTitleClick)
+
+// 4. (참고) 함수 내에서 선언된 변수는 함수 밖에서는 존재하지 않습니다.
+//그렇기 때문에 const currentColor로 변수 선언을 하더라도,
+//함수가 호출될 때 마다 새로운 값을 받을 수 있습니다.
