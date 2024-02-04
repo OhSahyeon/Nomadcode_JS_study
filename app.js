06a79e23a0bcc8fbf269a8813e5b5830997ff649
오승훈 <seunghun6257@gmail.com>
2024-02-04 18:33:10 +0900

    #3.4 Events part Two

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
-const title = document.getElementById("title")
+const title = document.querySelector("#title")
 console.dir(title)
-console.dir(title.localName)
 
-title.className = "p"
 
-document.getElementById("title")
+title.style.color = "blue"
+document.querySelector("#title").style.color = "red"
+
+
+title.addEventListener("mousewheel", handleTitleClick)
+
+function handleTitleClick () {
+  title.innerText = "Oh!"
+  title.style.transition = "3s"
+  title.style.color = "blue"
+  title.style.marginLeft = "150px"
+}
+
+console.dir(title)
