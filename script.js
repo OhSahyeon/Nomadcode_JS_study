39ff89418cba44dd9e217ef32762de9482ba8302
오승훈 <seunghun6257@gmail.com>
2024-02-06 23:29:48 +0900

    #4.2 Events-1

--- script.js
+++ script.js
@@ -1,16 +1,7 @@
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
 
 function clickLoginBtn(){
   const nameValue = loginInput.value
-  if (nameValue === ""){
-    alert("Please write your name")
-  } else if(nameValue.length > 5){
-    alert("Your name is to long...")
-  } else {
-    alert("thanks you!")
-    loginInput.value = ""
-  }
+  console.log(nameValue)
 };
-
-loginButton.addEventListener("click", clickLoginBtn);
