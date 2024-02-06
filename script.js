e802a0049f3a8c5ebb9bc14cc0ddd45f2ed5eeb2
오승훈 <seunghun6257@gmail.com>
2024-02-06 22:50:31 +0900

    #4.0 Input Values

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
-const title = document.querySelector("#title")
-console.dir(title)
+const loginInput = document.querySelector("#login-form input")
+const loginButton = document.querySelector("#login-form button")
 
-function handleTitleClick () { 
-  title.classList.toggle("on")
-  console.log(title.className)
+function clickLoginBtn(){
+  console.log(loginInput.value)
 }
 
-title.addEventListener("click", handleTitleClick)
-
-// 4. (참고) 함수 내에서 선언된 변수는 함수 밖에서는 존재하지 않습니다.
-//그렇기 때문에 const currentColor로 변수 선언을 하더라도,
-//함수가 호출될 때 마다 새로운 값을 받을 수 있습니다.
+loginButton.addEventListener("click", clickLoginBtn);
