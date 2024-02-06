594e1423fdd57ac0d6d5009864d47245ae1aa1c3
오승훈 <seunghun6257@gmail.com>
2024-02-06 23:26:10 +0900

    #4.1 Form Submission

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-const loginInput = document.querySelector("#login-form input")
-const loginButton = document.querySelector("#login-form button")
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
 
 function clickLoginBtn(){
-  console.log(loginInput.value)
-}
+  const nameValue = loginInput.value
+  if (nameValue === ""){
+    alert("Please write your name")
+  } else if(nameValue.length > 5){
+    alert("Your name is to long...")
+  } else {
+    alert("thanks you!")
+    loginInput.value = ""
+  }
+};
 
 loginButton.addEventListener("click", clickLoginBtn);
