e540df184fdec7c4a367825ec512b466f6333772
오승훈 <seunghun6257@gmail.com>
2024-02-07 20:45:14 +0900

    #4.2 Events

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 
-function clickLoginBtn(){
+function clickLoginBtn(e){
   const nameValue = loginInput.value
+  e.preventDefault()
+  console.log(e)
   console.log(nameValue)
 };
+
+loginForm.addEventListener("submit", clickLoginBtn)
