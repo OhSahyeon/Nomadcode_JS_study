d06123d96e1d1eafeae7b6ac471d3b829d850d18
오승훈 <seunghun6257@gmail.com>
2024-02-07 23:27:54 +0900

    #4.4 Getting Username

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const CLASSNAME_HIDDEN = "hidden"
 
 function clickLoginBtn(e){
-  const nameValue = loginInput.value
-  e.preventDefault()
-  console.log(e)
-  console.log(nameValue)
+  e.preventDefault();
+
+  const nameValue = loginInput.value;
+  console.log(nameValue);
+
+  loginForm.classList.add(CLASSNAME_HIDDEN);
+
+  greeting.innerText = `Hello ${nameValue}`;
+  greeting.classList.remove(CLASSNAME_HIDDEN)
 };
 
-loginForm.addEventListener("submit", clickLoginBtn)
+console.log(loginInput);
+
+loginForm.addEventListener("submit", clickLoginBtn);
