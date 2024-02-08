a0cdcffad4eb070e90845954289d45126029bdcb
오승훈 <seunghun6257@gmail.com>
2024-02-08 17:47:43 +0900

    #4.6 Loading Username

--- script.js
+++ script.js
@@ -2,22 +2,27 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
-const CLASSNAME_HIDDEN = "hidden"
+const CLASSNAME_HIDDEN = "hidden";
+const KEY_NAMEVALUE = "nameValue";
+const getUserName = localStorage.getItem(KEY_NAMEVALUE)
+
+loginForm.addEventListener("submit", clickLoginBtn);
+
+if (getUserName === null){
+  loginForm.classList.remove(CLASSNAME_HIDDEN)
+} else {
+  changeGreeting(getUserName)
+}
 
 function clickLoginBtn(e){
   e.preventDefault();
-
   const nameValue = loginInput.value;
-  console.log(nameValue);
-
   loginForm.classList.add(CLASSNAME_HIDDEN);
-
-  localStorage.setItem("nameValue", nameValue)
-  greeting.innerText = `Hello ${nameValue}`;
-  greeting.classList.remove(CLASSNAME_HIDDEN)
-  localStorage.getItem("nameValue")
+  localStorage.setItem(KEY_NAMEVALUE, nameValue)
+  changeGreeting(nameValue)
 };
 
-console.log(loginInput);
-
-loginForm.addEventListener("submit", clickLoginBtn);
+function changeGreeting(name){
+  greeting.innerText = `Hello ${name}`;
+  greeting.classList.remove(CLASSNAME_HIDDEN)
+}
