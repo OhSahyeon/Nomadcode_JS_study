0cc96716a9f0fa272d3bc3d552be315b9c178a27
오승훈 <seunghun6257@gmail.com>
2024-02-09 22:50:15 +0900

    #5.2 PadStart

--- JS/Clock.js
+++ JS/Clock.js
@@ -1,5 +1,12 @@
 const clock = document.querySelector("h2#clock");
 
-clock.innerText = "12:00"
+function getClock(){
+  const date = new Date();
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+  clock.innerText = `${hours}:${minutes}:${seconds}`;
+};
 
-setInterval(hello, 1000)
+getClock();
+setInterval(getClock, 1000);
