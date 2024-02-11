52728c4185dd8010d84caa22ea0648f8283ba6aa
오승훈 <seunghun6257@gmail.com>
2024-02-12 03:43:59 +0900

    #7.3 Saving To Dos

--- JS/Todo.js
+++ JS/Todo.js
@@ -2,10 +2,17 @@ const toDoForm = document.querySelector("#todo-form")
 const toDoInput = toDoForm.querySelector("input")
 const toDoList= document.querySelector("#todo-list")
 
+const toDo = [];
+
+function saveToDos() {
+  localStorage.setItem("todos", JSON.stringify(toDo))
+}
+
 function deleteToDo(e) {
   const thisLi = e.target.parentElement
   thisLi.remove()
 }
+
 
 function paintToDo(newToDo) {
   const li = document.createElement("li")
@@ -23,6 +30,8 @@ function toDoSubmit(e){
   e.preventDefault();
   const newToDo = toDoInput.value
   toDoInput.value = "";
+  toDo.push(newToDo)
+  saveToDos()
   paintToDo(newToDo)
 };
 
