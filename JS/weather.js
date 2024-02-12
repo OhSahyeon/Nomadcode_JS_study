2fe396282b7e0dea24eac1524875d4ef9033ffff
오승훈 <seunghun6257@gmail.com>
2024-02-13 00:30:48 +0900

    #8.1 Weather API

--- JS/weather.js
+++ JS/weather.js
@@ -1,7 +1,19 @@
+const API_KEY = "aeac64190fd598556a7df782f6898c8f"
+
 function Ok(p) {
   const lat= p.coords.latitude;
   const lng= p.coords.longitude;
   console.log(lat, lng)
+  const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+  fetch(URL).then(response => response.json()).then(data => {
+    console.log(data.name, data.weather[0].main)
+    const citySpan = document.querySelector("#weather span:first-child")
+    const weatherSpan = document.querySelector("#weather span:last-child")
+    const name = data.name;
+    const weather = data.weather[0].main;
+    citySpan.innerText = name
+    weatherSpan.innerText = weather
+  });
 }
 
 function No() {
